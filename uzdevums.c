9f998b2bc8045a984be53f848cc12163179b61ff
EriksKocuncs <112925824+EriksKocuncs@users.noreply.github.com>
2022-10-16 19:06:53 +0300

    40 000 000 000

---
+++ uzdevums.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    unsigned long int a, b, c;
+    printf("Ievadi 400000: \n");
+    scanf("%lu", &a); 
+    printf("Ievadi 100000: \n");
+    scanf("%lu", &b); 
+    c = a * b;
+    printf("\n%lu", c,"\n");
+
+    return 0;
+
+}
